d9e3196d95f654ed463a34161a8c906bbc25a0bb
Suhyeon <fbtngus2789@naver.com>
2024-12-24 21:46:00 +0900

    feat: implemented the Title component

--- frontend/src/views/Experiments.tsx
+++ frontend/src/views/Experiments.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 
+import Title from "../components/Title";
 import Indicator from "../components/Indicator";
 import DataTable from "../components/DataTable";
 import Unlearning from "../components/Unlearning";
@@ -57,10 +58,7 @@ export default function Experiments({
       className="p-1 relative border border-t-0 border-l-0"
     >
       <div className="flex justify-between items-center mb-1">
-        <div className="flex items-center ml-0.5">
-          <SettingsIcon className="scale-110" />
-          <h5 className="font-semibold ml-1 text-lg">Experiments</h5>
-        </div>
+        <Title Icon={<SettingsIcon />} title="Experiments" />
         {forgetClass !== undefined && (
           <Dialog
             open={open}
